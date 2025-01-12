0557101fd1f30b2e093b00cd2a58b5386b1d5400
shazzymaytryx <edwarddoku123@gmail.com>
2025-01-11 23:32:30 -0500

    minor changes on file

--- modules/vpc_module/main.tf
+++ modules/vpc_module/main.tf
@@ -1,17 +1,17 @@
 resource "aws_vpc" "my_vpc" {
-    cidr_block = var.vpc_cidr 
+  cidr_block = var.vpc_cidr
 }
 
 resource "aws_subnet" "private_subnet" {
-    vpc_id = aws_vpc.my_vpc.id
-    cidr_block = var.private_subnet
-  
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = var.private_subnet
+
 }
 
 resource "aws_subnet" "public_subnet" {
-    vpc_id = aws_vpc.my_vpc.id
-    cidr_block = var.public_subnet
-  
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = var.public_subnet
+
 }
 
 provider "aws" {
